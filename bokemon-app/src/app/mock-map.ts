abbf6f088ad8f38a577c50ece3ba94579f052ad4
AriaHV <aria.heijmensvisser@gmail.com>
2019-02-08 15:13:30 +0100

    Added a world view component using mock data.

---
+++ bokemon-app/src/app/mock-map.ts
@@ -0,0 +1,3 @@
+export const T1: string = "https://i.imgur.com/QVGC545.png";
+export const T2: string = "https://i.imgur.com/pGl4Sse.png";
+export const MAP: string[][] = [[T1, T2, T1],[T2, T2, T2],[T1, T2, T1]];
