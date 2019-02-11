c698532b88b3444251c62cb7a7e5fa857387b962
FrankFloris <frank_floris@hotmail.com>
2019-02-11 21:37:18 +0100

    commit maandag

--- bokemon-app/src/app/app-routing.module.ts
+++ bokemon-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorldViewComponent} from './world-view/world-view.component';
+import { LoginPageComponent} from './login-page/login-page.component';
+import { SignupComponent} from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login-page', component: LoginPageComponent},
+  {path: 'world-view', component: WorldViewComponent},
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
