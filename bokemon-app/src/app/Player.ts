a8e06811fa92342645830bb78f7a76dcdcc08263
FrankFloris <frank_floris@hotmail.com>
2019-02-14 09:43:17 +0100

    commit signup en login

--- bokemon-app/src/app/Player.ts
+++ bokemon-app/src/app/Player.ts
@@ -1,12 +1,21 @@
 
 export class Player {
+
   id: number = 0;
   username: string;
   password: string;
+  world: number;
+  sprite: string;
+  x: number;
+  y: number;
 
-  constructor(id: number, username: string, password: string) {
+  constructor(id: number, username: string, password: string, world: number, sprite: string, x: number, y: number) {
     this.id = id;
     this.username = username;
     this.password = password;
+    this.world = world;
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
   }
 }
